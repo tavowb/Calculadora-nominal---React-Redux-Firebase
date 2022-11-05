77ca6475119e559652f4464618739cfad26d95e1
Gustavo Jimenez <gusweb71@gmail.com>
2022-11-05 13:45:01 -0500

    Creo el reducer de la nomina

---
+++ src/reducers/nominaReducer.js
@@ -0,0 +1,16 @@
+/*
+
+{
+    id: "sadsadas"
+    fecha: "1/12/2022"
+    pago: 300.00
+}
+
+*/
+
+export const nominaReducer = (state = {}, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
