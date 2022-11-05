1aa9b9fad8b097b8c4dd5ac36067f47ef5c3f1d1
Gustavo Jimenez <gusweb71@gmail.com>
2022-11-05 15:20:29 -0500

    ahora el boton de agregar desplega un mini formulario en el cual se ingresara el pago y las horas trabajadas... se hace el calculo y todo se guarda en firebase

--- src/actions/nomina.js
+++ src/actions/nomina.js
@@ -1,12 +1,13 @@
 import { db } from "../firebase/config-firebase";
 
 /* Este archivo me va crear documentos, editarlos, borrarlos... */
-export const crearRegistro = () => {
+export const crearRegistro = ({ pago, horas }) => {
   return async (dispatch, getState) => {
     const { id } = getState().auth;
+    const calculo = pago * horas;
     const datos = {
       fecha: new Date(),
-      pago: 130.0,
+      pago: calculo,
     };
 
     const referencia = await db.collection(`${id}/nominas/nomina`).add(datos);
--- src/components/FormAdd.jsx
+++ src/components/FormAdd.jsx
@@ -1,19 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { crearRegistro } from "../actions/nomina";
 
 const FormAdd = () => {
   const dispatch = useDispatch();
 
+  const [viewForm, setViewForm] = useState(false);
+  const [CantidadPago, setCantidadPago] = useState({
+    pago: 0,
+    horas: 0,
+  });
+
+  const { pago, horas } = CantidadPago;
+
+  const handleChange = (e) => {
+    setCantidadPago({
+      ...CantidadPago,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   const handleAdd = () => {
-    dispatch(crearRegistro());
+    setViewForm(!viewForm);
+  };
+
+  const handleSave = () => {
+    dispatch(crearRegistro(CantidadPago));
   };
 
   return (
     <div>
-      <button className="btn btn-primary" onClick={handleAdd}>
-        Crear
+      <button className="btn " onClick={handleAdd}>
+        Agregar
       </button>
+      {viewForm && (
+        <>
+          <input
+            type="number"
+            placeholder="Ingrese la Cantidad de pago por hora"
+            name="pago"
+            value={pago}
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            placeholder="Ingrese la Cantidad de Horas trabajadas"
+            name="horas"
+            value={horas}
+            onChange={handleChange}
+          />
+
+          <button className="btn purple" onClick={handleSave}>
+            Calcular y Guardar
+          </button>
+        </>
+      )}
     </div>
   );
 };
